b91e3eb1b0c6ffad7745a653679440cd267f8c59
kr-prince <nphard12@gmail.com>
2021-10-21 15:52:15 +0530

    optimised code

--- BM25PlusVectorizer.py
+++ BM25PlusVectorizer.py
@@ -22,7 +22,7 @@ class BM25PlusVectorizer(object):
     ngram_range : (a,b) create ngrams from n in range(a,b+1) as features
     
     token_pattern : tokenizes the documents(strings) based on this pattern
-
+    
     tokenizer : user can pass a custom tokenizer function, then token_pattern has to be None 
     
     max_df : ignore terms which occur in #documents, greater than max_df threshold,
@@ -70,71 +70,74 @@ class BM25PlusVectorizer(object):
     assert len(tokens) >= n, "%d-grams not possible with %d tokens" % (n, len(tokens))
     return [' '.join(ngram) for ngram in zip(*[tokens[i:] for i in range(n)])]
   
-  def preprocess(self, corpus):
+  def process_corpus(self, corpus):
     """
-    Return a cleaned version of given corpus
+    Returns a cleaned and tokenized version of given corpus
     
     Parameter(s):
-      corpus : list of strings, where each string is considered a document
+      corpus : list of strings, or token list, where each sub-list element is considered a document
     """
     
-    self.vocabulary = set()
-    self.ngrams_count = Counter()  # for term frequency
-    self.doc_count = Counter()     # for document frequency
     clean_corpus = list()
-    self.mean_doc_len = 0
-    
     for document in corpus:
-      # remove recurrent spaces
-      document = re.sub(r'\s+', ' ', document.lower())
+      if type(document) is str:
+        # remove recurrent spaces in string
+        document = re.sub(r'\s+', ' ', document)
+      
       # tokenize on the basis of given pattern or custom tokenizer
       tokens = re.findall(self.token_pattern, document) if self.tokenizer is None else self.tokenizer(document)
       if self.stop_words is not None and len(self.stop_words) > 0:
         # filter stopwords
-        tokens = [token for token in tokens if token not in self.stop_words]
-
-      # create ngrams for later featurization and doc frequency
-      for n in range(self.ngram_range[0], self.ngram_range[1]+1):
-        ngrams = self.create_ngrams(tokens, n)
-        self.ngrams_count.update(ngrams)
-        self.doc_count.update(set(ngrams))
+        tokens = [token.lower() for token in tokens if token not in self.stop_words]
       
-      # create vocabulary
-      self.vocabulary.update(tokens)
-      clean_corpus.append(' '.join(tokens))
-      self.mean_doc_len += len(tokens)
-      
-    self.mean_doc_len /= len(corpus)
+      clean_corpus.append(tokens)
+    
     return clean_corpus
 
   def fit(self, corpus):
     """
-    Fits the BM25Plus vectorizer over given corpus
+    Fits the BM25Plus vectorizer over given corpus and prepares the features
     
     Parameter(s):
-      corpus : list of strings, where each string is considered a document
+      corpus : list of strings, or token list, where each sub-list element is considered a document
     """
     
-    self.clean_corpus = self.preprocess(corpus)
-    self.idf_score = dict()
-    # calculate min_df and max_df thresholds from their given values
+    self.vocabulary = set()        # for corpus vocabulary - only tokens
+    self.ngrams_count = Counter()  # for term frequency across corpus
+    self.doc_count = Counter()     # for term-document count
+    clean_corpus = self.process_corpus(corpus)
+    self.mean_doc_len = 0
+    
+    for doc_tokens in clean_corpus:
+      # create ngrams for later featurization, set term frequency and term-doc frequency
+      for n in range(self.ngram_range[0], self.ngram_range[1]+1):
+        ngrams = self.create_ngrams(doc_tokens, n)
+        self.ngrams_count.update(ngrams)
+        self.doc_count.update(set(ngrams))
+      
+      self.vocabulary.update(doc_tokens)
+      self.mean_doc_len += len(doc_tokens)
+    self.mean_doc_len /= len(corpus)
+    
+    # calculate min_df and max_df actual thresholds from their given values
     min_df_thres = self.min_df if type(self.min_df) is int else int(self.min_df*len(corpus))
     max_df_thres = self.max_df if type(self.max_df) is int else int(self.max_df*len(corpus))
     
-    for phrase in self.ngrams_count.keys():
+    self.idf_score = dict()
+    self.features, self.feat_id_map = list(), dict()
+    # sort n_grams as per their frequency across corpus, filter on max_features cap, filter 
+    # on given max_df and min_df values and calculate their idf scores. They will be features. 
+    for phrase,_ in self.ngrams_count.most_common(self.max_features):
       doc_count = self.doc_count.get(phrase, 0)
       if doc_count >= min_df_thres and doc_count <= max_df_thres:
         # calculate idf score 
         self.idf_score[phrase] = math.log(((len(corpus)-doc_count+0.5)/(doc_count+0.5)) +1)
-    
-    # get the features as per given limit
-    self.features = [phrase for phrase,count in self.ngrams_count.most_common() if phrase in self.idf_score]
-    if self.max_features is not None:
-      self.features = self.features[:self.max_features]
+        self.feat_id_map[phrase] = len(self.features)
+        self.features.append(phrase)
 
-  def transform(self):
+  def transform(self, query_corpus):
     """
-    Transforms the given corpus
+    Transforms the given query corpus, based on the features formed over fitted corpus
     
     Parameter(s):
       None
@@ -142,25 +145,31 @@ class BM25PlusVectorizer(object):
     
     assert hasattr(self, 'features'), "BM25+ Vectorizer has to be fitted first, run fit() method."
     
+    clean_qcorpus = self.process_corpus(query_corpus)
     row_i, col_i, bm25p_score = list(), list(), list()
-    for di,document in enumerate(self.clean_corpus):
-      for fi,feat in enumerate(self.features):
-        feat_count = len(re.findall('\\b%s\\b'%feat, document))
-        if feat_count > 0:
-          # calculate the term frequency ratio score
-          doc_len, feat_len = len(document.split()), len(feat.split())
-          total_count = doc_len - feat_len + 1.0
-          tf_ratio = feat_count / total_count
-          numerator = tf_ratio * (self.k1 + 1.0)
-          denominator = tf_ratio + self.k1 * (1.0 - self.b + (self.b * doc_len/self.mean_doc_len))
-          tf_score = (numerator/denominator) + self.delta
-          row_i.append(di)
-          col_i.append(fi)
-          bm25p_score.append(tf_score * self.idf_score[feat])
+    for di,doc_tokens in enumerate(clean_qcorpus):
+      # featurize this document
+      ngrams_doc_freq = Counter()
+      for n in range(self.ngram_range[0], self.ngram_range[1]+1):
+        ngrams = self.create_ngrams(doc_tokens, n)
+        ngrams_doc_freq.update([ng for ng in ngrams if self.feat_id_map.get(ng) is not None])
+      
+      for feat, feat_count in ngrams_doc_freq.items():
+        # calculate the term frequency ratio score and normalize it based on given factors
+        doc_len, feat_len = len(doc_tokens), len(feat.split())
+        total_count = doc_len - feat_len + 1.0
+        tf_ratio = feat_count / total_count
+        numerator = tf_ratio * (self.k1 + 1.0)
+        denominator = tf_ratio + self.k1 * (1.0 - self.b + (self.b * doc_len/self.mean_doc_len))
+        tf_score = (numerator/denominator) + self.delta
+        row_i.append(di)
+        col_i.append(self.feat_id_map[feat])
+        bm25p_score.append(tf_score * self.idf_score[feat])
     
-    bm25p_matrix = csr_matrix((bm25p_score, (row_i, col_i)), shape=(len(self.clean_corpus),
-                                                            len(self.features)), dtype=float)
+    bm25p_matrix = csr_matrix((bm25p_score, (row_i, col_i)), shape=(len(clean_qcorpus),
+                                                    len(self.features)), dtype=float)
     return bm25p_matrix
+
 
 if __name__ == '__main__':
   corpus = [
@@ -172,6 +181,6 @@ if __name__ == '__main__':
 
   bm25p = BM25PlusVectorizer(stop_words=['this','is', 'the'], ngram_range=(1,2), max_features=3)
   bm25p.fit(corpus)
-  print(bm25p.transform().toarray())
+  print(bm25p.transform(corpus).toarray())
   print(bm25p.features)
   print(bm25p.ngrams_count)
